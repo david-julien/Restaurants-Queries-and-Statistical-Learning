708f5b7ec3dcde3b37067dbaffefeb0b7307f18d
arnldlm <arnoldlam1@gmail.com>
2015-11-26 21:21:43 -0800

    initial commit

--- src/ca/ece/ubc/cpen221/mp5/Review.java
+++ src/ca/ece/ubc/cpen221/mp5/Review.java
@@ -1,7 +1,14 @@
 package ca.ece.ubc.cpen221.mp5;
 
+import java.util.*;
+import org.json.simple.*;
+
 // TODO: Use this class to represent a Yelp review.
 
 public class Review {
-
+	
+	
+	public Review(JSONObject reviewJSON) {
+		
+	}
 }
